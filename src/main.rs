e3a04f33029ae20ac51b11ee3d5d848142fa9485
thodoris <thodoris@spotify.com>
2021-11-26 17:11:08 +0100

    Added a loops function

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ fn main() {
     numerical_operations();
     tuple();
     control_flow();
+    loops();
 }
 
 fn message() -> String {
@@ -94,4 +95,27 @@ fn control_flow() {
     let number = if condition { 5 } else { 6 };
 
     println!("The value of number is: {}", number);
+}
+
+fn loops() {
+    println!("-----LOOPS----");
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {}", count);
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {}", remaining);
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {}", count);
 }
